c18d92ef2fefec26da1ee220be6eb0713966e11a
Mladjo Milunovic <mladjo.milunovic95@gmail.com>
2023-12-01 14:17:21 +0100

    day 1 better

--- day_01/day_01_better.dart
+++ day_01/day_01_better.dart
@@ -4,20 +4,20 @@ main() {
   final lines = input.split("\n");
 
   final nameToDigit = {
-    "zero": 0,
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
   };
 
   int lineValue(String line, {required bool onlyNumbers}) {
-    final toMatch = nameToDigit.values.map((e) => e.toString()).toList();
+    final toMatch = nameToDigit.values.toList();
     if (!onlyNumbers) {
       toMatch.addAll(nameToDigit.keys);
     }
