8d035afaae0978e4d821a2ea86a5480846bb1349
Mladjo Milunovic <mladjo.milunovic95@gmail.com>
2023-12-06 06:36:20 +0100

    day_06

---
+++ day_06/day_06.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+import 'package:collection/collection.dart';
+
+import 'input.dart';
+
+void main() {
+  final lines = input.split("\n");
+  var matches = RegExp(r'\d+').allMatches(lines[0]);
+  final durations = matches.map((e) => int.parse(e.group(0)!)).toList();
+  matches = RegExp(r'\d+').allMatches(lines[1]);
+  final records = matches.map((e) => int.parse(e.group(0)!)).toList();
+
+  final p1 = durations
+      .mapIndexed((i, e) => nSolutions(e, records[i]))
+      .reduce((value, element) => value * element);
+  print(p1);
+
+  final duration = int.parse(durations.map((e) => e.toString()).join());
+  final record = int.parse(records.map((e) => e.toString()).join());
+  final p2 = nSolutions(duration, record);
+  print(p2);
+}
+
+int nSolutions(int duration, int record) {
+  //holdTime^2 - holdTime * duration + record < 0
+  final b = -duration;
+  final c = record;
+  var root1 = (-b + sqrt(pow(b, 2) - 4 * c)) / 2;
+  var root2 = (-b - sqrt(pow(b, 2) - 4 * c)) / 2;
+
+  final smallerRoot = min(root1, root2);
+  final largerRoot = max(root1, root2);
+
+  final nIntSolution = largerRoot.floor() - smallerRoot.ceil() + 1;
+  return nIntSolution;
+}
---
+++ day_06/input.dart
@@ -0,0 +1,5 @@
+const testInput = r'''Time:      7  15   30
+Distance:  9  40  200''';
+
+const input = r'''Time:        42     68     69     85
+Distance:   284   1005   1122   1341''';
